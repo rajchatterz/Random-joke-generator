76d620c2f045465e83931ac2c30fe971820383b3
rajchatterz <iamraj.space@gmail.com>
2023-10-24 01:13:22 +0530

    done with the jike generator

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const setup = document.getElementById('setup')
 const delivery = document.getElementById('delivery')
 const url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=twopart'
-
-fetch(url).then(res => {
-    if (!res.ok) {
-        throw new Error(`Http error! status: ${res.error}`)
-    }
-    return res.json()
-}).then(data => {
-    const joke = data.setup
-    const ans = data.delivery
-    setup.innerHTML = joke
-    delivery.innerHTML=ans
-}).catch(error => {
-    console.log(error)
+const btn = document.getElementById('btn')
+btn.addEventListener("click", () => {
+    fetch(url).then(res => {
+        if (!res.ok) {
+            throw new Error(`Http error! status: ${res.error}`)
+        }
+        return res.json()
+    }).then(data => {
+        const joke = data.setup
+        const ans = data.delivery
+        setup.innerHTML = joke
+        delivery.innerHTML=ans
+    }).catch(error => {
+        console.log(error)
+    })
 })
